3b20463cb2fa0961dafc0e82c4a387f8d04b7a86
tbor00 <btimoteo@outlook.com>
2021-11-26 18:47:39 -0300

    commented navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
             <Navbar.Brand onClick={() => history.push('/')} className="cursor-pointer">
                 LIBREGOL
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#features">Features</Nav.Link>
@@ -21,7 +21,7 @@ const NavBar = () => {
                         Dank memes
                     </Nav.Link>
                 </Nav>
-            </Navbar.Collapse>
+            </Navbar.Collapse> */}
         </Navbar>
     )
 }
